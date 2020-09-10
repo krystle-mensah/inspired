9a1d3968bcb22a2e2c281d05181b75237cbba039
Krystle Mensah <krystlemensah@Krystles-MBP.cust.communityfibre.co.uk>
2020-09-10 20:27:37 +0100

    update

--- inc/header.php
+++ inc/header.php
@@ -1,25 +1,31 @@
 <header class="blog-header py-3 site_header">
-  <!-- <div class="row flex-nowrap justify-content-between align-items-center"> -->
+  
 
     <div class="col-4 pt-1">
       <a class="sub-button" href="#">Subscribe</a>
     </div>
 
-    <div class="col-4 text-center">
-      <a class="blog-header-logo text-dark" href="#"><img src="main-img/logo.png" alt="inspired sound"></a>
+    <div class="logo_container">
+      
+        <a class="header_logo" href="#"><img src="main-img/logo.png" alt="inspired sound"></a>
+      
     </div>
+    
+    <div class="header_left">
 
-    <div class="header_share col-4 d-flex justify-content-end align-items-center">
-      <a href="#"><i class="fab fa-facebook-f"></i></a>
-      <a href="#"><i class="fab fa-twitter"></i></a>
-      <a href="#"><i class="fab fa-youtube"></i></a>
-    </div>
-    <div class="search_box" href="#">
-        <input class="search_input" type="" placeholder="Search">
-        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg> -->
-        <button class="search_button">
-          <i class="fas fa-search"></i>
-        </button>
-      </div>
-  <!-- </div> -->
+      <!-- <div class="header_share">
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+      </div> -->
+
+      <form class="form-inline mt-2 mt-md-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+
+    </div><!-- header_left --> 
+    
+  
 </header>
