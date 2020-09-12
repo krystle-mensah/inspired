dbbfa36f908f672a7574ea546eff38f989c771a1
Krystle Mensah <krystlemensah@Krystles-MBP.cust.communityfibre.co.uk>
2020-09-12 13:42:58 +0100

    styling

--- admin/inc/admin_nav.php
+++ admin/inc/admin_nav.php
@@ -1,4 +1,3 @@
-<!-- TOP NAV -->
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
 			<a class="navbar-brand" href="index.html">Welcome to Admin</a>
 			<button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
