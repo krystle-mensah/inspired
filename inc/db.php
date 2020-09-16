4f50297b3bf66fbf9e7a3169ee0f896d8390fad4
Krystle Mensah <krystlemensah@Krystles-MBP.cust.communityfibre.co.uk>
2020-09-16 08:38:33 +0100

    connection error

--- inc/db.php
+++ inc/db.php
@@ -17,9 +17,9 @@ foreach($db as $key => $value){
 $connection = new mysqli(DB_HOST, DB_USER,DB_PASS,DB_NAME);
 
 // Check connection
-if ($connection->connect_error) {
-  die("Connection failed: " . $connection->connect_error);
-}
-echo "Connected successfully";
+// if ($connection->connect_error) {
+//   die("Connection failed: " . $connection->connect_error);
+// }
+// echo "Connected successfully";
 
 ?>
