c990c8cab0ed3198acf23afc055f7a8eebcde84e
Krystle Mensah <krystlemensah@Krystles-MBP.cust.communityfibre.co.uk>
2020-09-25 21:20:17 +0100

    logout page

--- admin/inc/admin_nav.php
+++ admin/inc/admin_nav.php
@@ -23,7 +23,7 @@
 						<a class="dropdown-item" href="#">Settings</a>
 						<a class="dropdown-item" href="#">Activity Log</a>
 						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="login.html">Logout</a>
+						<a class="dropdown-item" href="../inc/logout.php">Logout</a>
 					</div>
 				</li>
 			</ul>
