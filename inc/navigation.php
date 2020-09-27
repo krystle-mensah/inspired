5d1036c14c5f91129505a5f5917ae0a98a2e5ef0
Krystle Mensah <krystlemensah@Krystles-MBP.cust.communityfibre.co.uk>
2020-09-27 11:15:30 +0100

    contact nav

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,19 +1,12 @@
-<?php
-  //select all from table
-  $query = "SELECT * FROM categories";
-  // send that all in
-  $select_categories_navigation = mysqli_query($connection,$query);				
-?>
+<?php $sql = $connection->query( "SELECT * FROM categories" ); ?>
   <hr>
   <nav class="nav d-flex justify-content-between site-navigation">
     <div><a class="" href="index.php">home</a></div>
-    <?php
-    while($row = mysqli_fetch_array($select_categories_navigation)) {
+    <?php foreach($sql as $row) :
       $cat_title = $row['cat_title'];
       $cat_id = $row['cat_id'];
-
-      echo "<div><a class='' href='category.php?category=$cat_id '>{$cat_title}</a></div>";
-    }
-    ?>
+      echo "<div><a href='category.php?category=$cat_id'>{$cat_title}</a></div>";
+    endforeach; ?>
+    <div><a class="" href="contact.php">contract</a></div>
   </nav>
   <hr>
