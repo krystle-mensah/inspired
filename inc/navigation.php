2347d7531c9353342fde1334521d320b054e2868
krystle Mensah <web-development@krystlemensah.co.uk>
2021-01-04 10:25:39 +0000

    update

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,12 +1,24 @@
 <?php $sql = $connection->query( "SELECT * FROM categories" ); ?>
   <hr>
-  <nav class="nav d-flex justify-content-between site-navigation">
-    <div><a class="" href="index.php">home</a></div>
-    <?php foreach($sql as $row) :
-      $cat_title = $row['cat_title'];
-      $cat_id = $row['cat_id'];
-      echo "<div><a href='category.php?category=$cat_id'>{$cat_title}</a></div>";
-    endforeach; ?>
-    <div><a class="" href="contact.php">contact</a></div>
+  <nav class="nav justify-content-between site-navigation">
+    <a class="" href="index.php">home</a>
+
+      <?php foreach($sql as $row) :
+        $cat_title = $row['cat_title'];
+        $cat_id = $row['cat_id'];
+       
+
+          echo "
+          
+          
+            <div><a class='dropbtn' href='category.php?category=$cat_id'>{$cat_title}</a></div>
+          
+          
+          
+          ";
+      
+      endforeach; ?>
+
+    <a class="" href="contact.php">contact</a>
   </nav>
   <hr>
