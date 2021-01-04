143b67084462093ceecfd2b75802d0f5ebf374b6
krystle Mensah <web-development@krystlemensah.co.uk>
2021-01-04 11:39:29 +0000

    styles for navigation

--- inc/navigation.php
+++ inc/navigation.php
@@ -9,16 +9,29 @@
        
 
           echo "
-          
-          
-            <div><a class='dropbtn' href='category.php?category=$cat_id'>{$cat_title}</a></div>
-          
-          
-          
-          ";
+            <div>
+              <a href='category.php?category=$cat_id'>
+                <div class='drop_down'>
+                  <button class='drop_btn'>{$cat_title}</button>
+                    <div class='dropdown_content'>";
+                    $selectAll_chapters = $connection->query( "SELECT * FROM chapters" );
+                    foreach( $selectAll_chapters as $row){
+                      $chapter_title = $row['chapterName'];
+                      $chapterId = $row['chapterId'];
+echo "";
+                    }
+                      "
+                      <a href='#'>Link 1</a>
+                      <a href='#'>Link 1</a>
+                      <a href='#'>Link 1</a>
+                    </div>
+                </div>
+              </a>
+            </div>
+              ";
       
-      endforeach; ?>
-
+        endforeach; 
+      ?>
     <a class="" href="contact.php">contact</a>
   </nav>
   <hr>
