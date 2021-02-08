d32d2326d82b41e6bdfd37f7cefb3a57978fc36a
krystle Mensah <web-development@krystlemensah.co.uk>
2021-02-08 09:07:15 +0000

    page tidy

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php 
-
 include "inc/db.php"; 
 include "inc/head.php"; 
 include "inc/header.php";
-
 ?>
 
 <div class="container">
@@ -17,49 +15,42 @@ include "inc/header.php";
       <div class="row">
         <div class="col-md-8 blog_main">
           <div class="row h-100">
-         
             <?php $sql = $connection->query("SELECT * FROM `posts` ORDER BY `posts`.`post_date` DESC"); 
-            
             foreach($sql as $row){
             
             $post_category_id = $row['post_category_id'];
             $post_title = $row['post_title'];
             $post_author = $row['post_author'];
             $post_date = $row['post_date'];
-            // reference form the database
             $post_image = $row['post_image'];
           
             ?>
-            <!-- on this page I am requseting the post page -->
             <a class="post_link" href="post.php?p_id=<?= $row['post_id']; ?>">
               <div class="card col-md-6 ">
-                    <div class="blog-post">
-                      <img class="flex-auto d-md-block post-image img-fluid card-img-top" src="img/<?= $post_image; ?>" alt="Card image cap">
-                      <div class="card-body d-flex flex-column">
-                        <div class="post-content">
-                          <h1 class="post_title"><?= $post_title;  ?></h1>
-                          <p class="blog-post-meta post_date"><?= $post_date;  ?> by <a class="post_author" href="#"><?= $post_author;  ?></a></p>
-                          <?php 
-                          $request_to = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
-                          $select_categories_id = mysqli_query($connection,$request_to);
-                          foreach($select_categories_id as $number_key):
-                            $cat_id = $number_key['cat_id'];
-                            $cat_title = $number_key['cat_title'];
-                          endforeach;
-                          ?>
-                          <a class="post_cat_title" href="category.php?category=<?= $cat_id;  ?>">
-                            <p class="card-text"><strong><?= $cat_title;  ?></strong></p>
-                          </a>
-                        </div><!-- post-content -->
-                      </div><!-- card body -->
-                    </div><!-- blog-post --> 
-                <!-- </div> -->
-                <!-- alignment -->
-
+                <div class="blog-post">
+                  <img class="flex-auto d-md-block post-image img-fluid card-img-top" src="img/<?= $post_image; ?>" alt="Card image cap">
+                  <div class="card-body d-flex flex-column">
+                    <div class="post-content">
+                      <h1 class="post_title"><?= $post_title;  ?></h1>
+                      <p class="blog-post-meta post_date"><?= $post_date;  ?> by <a class="post_author" href="#"><?= $post_author;  ?></a></p>
+                      <?php 
+                      $request_to = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
+                      $select_categories_id = mysqli_query($connection,$request_to);
+                      foreach($select_categories_id as $number_key):
+                        $cat_id = $number_key['cat_id'];
+                        $cat_title = $number_key['cat_title'];
+                      endforeach;
+                      ?>
+                      <a class="post_cat_title" href="category.php?category=<?= $cat_id;  ?>">
+                        <p class="card-text"><strong><?= $cat_title;  ?></strong></p>
+                      </a>
+                    </div><!-- post-content -->
+                  </div><!-- card body -->
+                </div><!-- blog-post --> 
               </div><!-- card -->
             </a><!-- post link -->
             <?php } ?>
-          </div>
+          </div><!-- row -->
         </div><!-- alignment and main blog -->
       
         <!-- SIDEBAR -->
