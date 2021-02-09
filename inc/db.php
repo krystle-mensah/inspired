257748679d8c929147e661332ed05a5c38f3f077
krystle Mensah <web-development@krystlemensah.co.uk>
2021-02-09 12:30:38 +0000

    pdoTutorial

--- inc/db.php
+++ inc/db.php
@@ -1,26 +1,54 @@
 <?php
 
-// db array assigned to value's
+// NEW DATABASE CONNECTION
+// Class Database{
+ 
+// 	private $server = "mysql:host=localhost;dbname=inspired";
+// 	private $username = "root";
+// 	private $password = "";
+// 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+// 	protected $conn;
+ 	
+// 	public function open(){
+//  		try{
+//  			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
+//  			return $this->conn;
+//  		}
+//  		catch (PDOException $e){
+//  			echo "There is some problem in connection: " . $e->getMessage();
+//  		}
+ 
+//     }
+ 
+// 	public function close(){
+//    		$this->conn = null;
+//  	}
+ 
+// }
+
+// $pdo = new Database();
+
+
+//db array assigned to value's
 $db['db_host'] = "localhost";
 $db['db_user'] = "root";
 $db['db_pass'] = "";
 $db['db_name'] = "inspired";
 
-// foreach db array use key to loop through each value.
+//foreach db array use key to loop through each value.
 foreach($db as $key => $value){
   
   define(strtoupper($key),$value);
 
 }
 
-// Create connection
+//Create connection
 $connection = new mysqli(DB_HOST, DB_USER,DB_PASS,DB_NAME);
 
-// Check connection
+//Check connection
 // if ($connection->connect_error) {
 //   die("Connection failed: " . $connection->connect_error);
 // }
 // echo "Connected successfully";
-
 
 ?>
