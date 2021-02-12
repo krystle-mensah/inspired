79420c6384de1bd43b352b0213b2c913366103c4
krystle Mensah <web-development@krystlemensah.co.uk>
2021-02-12 20:57:12 +0000

    sqli to pdo

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,32 +1,35 @@
-<?php $all_categories = $connection->query( "SELECT * FROM categories" ); ?>
-  <hr class="hide_on_small">
-  <nav class="nav justify-content-between site-navigation hide_on_small">
-    <a class="nav_link" href="index.php">home</a>
+<?php $all_categories = $connection->query("SELECT * FROM categories"); ?>
+<hr class="hide_on_small">
+<nav class="nav justify-content-between site-navigation hide_on_small">
+  <a class="nav_link" href="index.php">home</a>
 
-      <?php foreach($all_categories as $row) { ?>
-      <?php 
-        $cat_title = $row['cat_title'];
-        $cat_id = $row['cat_id'];
-        
-            echo "<div>";
-              echo" <a class='nav_link' href='category.php?category=$cat_id'>";
-                echo "<div class='drop_down'>";
-                  echo "<button class='drop_btn'>$cat_title</button>";
-                    echo "<div class='dropdown_content'>";
-                    $sql = $connection->query("SELECT * FROM `sub_categories` WHERE subCategoriesCatID = {$row['cat_id']}"); 
-                    foreach($sql as $row) {
+  <?php foreach ($all_categories as $row) { ?>
+  <?php
+    $cat_title = $row['cat_title'];
+    $cat_id = $row['cat_id'];
 
-                      echo "<a class='dropdown_nav_link' href='sub_category.php?subCategory=$row[subCategoriesID]'>$row[subCategoriesTitle]</a>";
+    echo "<div>";
+    echo " <a class='nav_link' href='category.php?category=$cat_id'>";
+    echo "<div class='drop_down'>";
+    echo "<button class='drop_btn'>$cat_title</button>";
+    echo "<div class='dropdown_content'>";
 
-                    }
-                      echo "</div>";
-                    echo "</div>";
-              echo "</a>";
-            echo "</div>";
+    $sql = "SELECT * FROM `sub_categories` WHERE subCategoriesCatID =  :cat_id";
+    $stmt = $connection->prepare($sql);
+    $stmt->execute(['subCategoriesCatID' => $cat_id]);
+    $getSubCatID = $stmt->fetchAll();
+    //$sql = $connection->query("SELECT * FROM `sub_categories` WHERE subCategoriesCatID = {$row['cat_id']}");
+    foreach ($stmt as $row) {
+
+      echo "<a class='dropdown_nav_link' href='sub_category.php?subCategory=$row[subCategoriesID]'>$row[subCategoriesTitle]</a>";
     }
+    echo "</div>";
+    echo "</div>";
+    echo "</a>";
+    echo "</div>";
+  }
 
-?>
-    <a class="nav_link" href="contact.php">contact</a>
-  </nav><!-- site-navigation -->
-  <hr class="hide_on_small">
-
+  ?>
+  <a class="nav_link" href="contact.php">contact</a>
+</nav><!-- site-navigation -->
+<hr class="hide_on_small">
