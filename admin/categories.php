207a5346bdf5cfbdad4c8ae690055f42298a57b3
krystle Mensah <web-development@krystlemensah.co.uk>
2021-02-15 23:01:29 +0000

    prepared statments

--- admin/categories.php
+++ admin/categories.php
@@ -1,158 +1,145 @@
 <?php include "inc/admin_head.php" ?>
-	<body class="sb-nav-fixed">
-		<!-- TOP NAV -->
-		<?php include "inc/admin_nav.php" ?>
-		<!-- PAGE WRAPPER -->
-		<div id="layoutSidenav">
-			<div class="container-fluid">
-				<div class="row">
-					<!-- SIDE NAV -->
-					<?php include "inc/admin_sideNav.php" ?>
-					<!-- CONTENT -->
-					<div id="layoutSidenav_content">
-						<main>
-							<div class="container-fluid">
-								<div class="row">
-									<div class="col-lg-12">
-										<!-- PAGE TITLE -->
-										<h1 class="mt-4">Categories</h1>
-									</div><!-- alignment -->
-									<!--CREATE A CATEGORY  -->
-									<?php 
 
-										if(isset($_POST['submit'])) {
+<body class="sb-nav-fixed">
+	<!-- TOP NAV -->
+	<?php include "inc/admin_nav.php" ?>
+	<!-- PAGE WRAPPER -->
+	<div id="layoutSidenav">
+		<div class="container-fluid">
+			<div class="row">
+				<!-- SIDE NAV -->
+				<?php include "inc/admin_sideNav.php" ?>
+				<!-- CONTENT -->
+				<div id="layoutSidenav_content">
+					<main>
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-lg-12">
+									<!-- PAGE TITLE -->
+									<h1 class="mt-4">Categories</h1>
+								</div><!-- alignment -->
+								<!--CREATE A CATEGORY  -->
+								<?php
 
-											$cat_title = $_POST['cat_title'];
+								if (isset($_POST['submit'])) {
 
-											//// if cat_title is equal to empty string or function to check is var is empty
-											if($cat_title == "" || empty($cat_title)) {
-												//// Then display this.
-												echo "This field should not empty";
+									$cat_title = $_POST['cat_title'];
 
-											}else{
+									//// if cat_title is equal to empty string or function to check is var is empty
+									if ($cat_title == "" || empty($cat_title)) {
+										//// Then display this.
+										echo "This field should not empty";
+									} else {
 
-												// insert what user inputs to the categories table and column. 
-												$query = "INSERT INTO categories(cat_title)";
-												
-												// and assign value to variable. 
-												$query .= "VALUE('{$cat_title}')";
+										$insertQry = 'INSERT INTO categories (cat_title) VALUE(?)';
+										$insertStatement = mysqli_prepare($connection, $insertQry);
+										mysqli_stmt_bind_param($insertStatement, 's', $cat_title);
+										mysqli_stmt_execute($insertStatement);
 
-												// then send to database with the connection and query. 
-												$create_category_query = mysqli_query($connection, $query);
-											
-												// check if query was succesful
-												if(!$create_category_query) {
-													
-													// terminate script and display error with the connection. 
-													die('QUERY FAILED' . mysqli_error($connection));
+										if (!$insertStatement) {
 
-												}
+											// terminate script and display error with the connection. 
+											die('QUERY FAILED' . mysqli_error($connection));
+										}
+									} // End else
 
-											} // End else
+								} // isset function
 
-										} // isset function
+								?>
+
+								<div class="col-xs-6 col-lg-4">
+									<!-- ADD CATEGORY -->
+									<form action="" method="post">
+
+										<div class="form-group">
+											<label for="cat-title">Add Category</label>
+											<input type="text" name="cat_title" class="form-control">
+										</div><!-- form-group -->
+
+										<!-- SUBIT BUTTON -->
+										<div class="form-group">
+											<input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+										</div><!-- form-group -->
+									</form><!-- form -->
+
+									<?php // UPDATE AND INCLUDE QUERY
+
+									// DECTECT - if the edited link is declared 
+									if (isset($_GET['edit'])) {
+
+										// IF TRUE - ASSIGN TO cat_id. 
+										$cat_id = $_GET['edit'];
+
+										// PATH TO UPDATE_CATEGORIES.PHP
+										include "inc/update_categories.php";
+									}
 
 									?>
+								</div><!-- alignment -->
 
-									<div class="col-xs-6 col-lg-4">
-										<!-- ADD CATEGORY -->
-										<form action="" method="post">
-
-											<div class="form-group">
-												<label for="cat-title">Add Category</label>
-												<input type="text" name="cat_title" class="form-control">
-											</div><!-- form-group -->
-											
-											<!-- SUBIT BUTTON -->
-											<div class="form-group">
-												<input class="btn btn-primary" type="submit" name="submit" value="Add Category">
-											</div><!-- form-group -->
-										</form><!-- form -->
-
-										<?php // UPDATE AND INCLUDE QUERY
-
-											// DECTECT - if the edited link is declared 
-											if(isset($_GET['edit'])){
-
-												// IF TRUE - ASSIGN TO cat_id. 
-												$cat_id = $_GET['edit'];
-
-												// PATH TO UPDATE_CATEGORIES.PHP
-												include "inc/update_categories.php";
-
-											}
-
-										?>
-									</div><!-- alignment -->
-
-									<div class="col-xs-6 col-lg-12">
-										<table class="table table-striped table-bordered table-hover my_table">
-											<thead>
-												<tr>
-													<th>Id</th>
-													<th>Category Title</th>
-												</tr>
-											</thead>
-											<tbody>
+								<div class="col-xs-6 col-lg-12">
+									<table class="table table-striped table-bordered table-hover my_table">
+										<thead>
+											<tr>
+												<th>Id</th>
+												<th>Category Title</th>
+											</tr>
+										</thead>
+										<tbody>
 											<?php
-												$query = "SELECT * FROM categories";
-												$select_categories = mysqli_query($connection,$query);
+											$query = "SELECT * FROM categories";
+											$select_categories = mysqli_query($connection, $query);
 
-												// while the condition is true fetch the row representing the array from ($variable)
-												while($row = mysqli_fetch_array($select_categories)) {
-													// Then assign the array to a variable
-													$cat_id = $row['cat_id'];
-													$cat_title = $row['cat_title'];
+											// while the condition is true fetch the row representing the array from ($variable)
+											while ($row = mysqli_fetch_array($select_categories)) {
+												// Then assign the array to a variable
+												$cat_id = $row['cat_id'];
+												$cat_title = $row['cat_title'];
 
-													echo "<tr>";
-													// Then display the fetch row form the database in the browser. 
-														echo "<td>{$cat_id}</td>";
-														echo "<td>{$cat_title}</td>";
-														// delete button
-														echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-														// Edited link
-														echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-													echo "</tr>";
-												}
+												echo "<tr>";
+												// Then display the fetch row form the database in the browser. 
+												echo "<td>{$cat_id}</td>";
+												echo "<td>{$cat_title}</td>";
+												// delete button
+												echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+												// Edited link
+												echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+												echo "</tr>";
+											}
 
 											?>
 
 											<!-- DELETE QUERY FUNCTION -->
-											
-											<?php 
 
-												//check for the delete key in url
-												if(isset($_GET['delete'])) {
-													// then save it here
-													$the_cat_id = $_GET['delete'];
-											
-													// query to delete from categories where, then we reference the column in the database which is called cat_id equal to the $the_cat_id
-													$query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
-											
-													// send in
-													$delete_query = mysqli_query($connection, $query);
-											
-													// once the above is done refrash in this location
-													header("Location: categories.php");
-													
-												}
-		
-											?>	
-										
-											</tbody>
-										</table> <!-- table table-bordered table-hover -->
+											<?php
 
-									</div><!-- alignment -->
-								</div><!-- row -->
-							</div><!-- container fluid -->
-						</main><!-- main -->
+											if (isset($_GET['delete'])) {
+												$the_cat_id = $_GET['delete'];
 
-					</div> <!-- content -->
+												$deleteQry = "DELETE FROM categories WHERE cat_id = ?";
+												$deleteStatement = mysqli_prepare($connection, $deleteQry);
+												mysqli_stmt_bind_param($deleteStatement, 'i', $the_cat_id);
+												mysqli_stmt_execute($deleteStatement);
 
-				</div><!-- row -->
+												header("Location: categories.php");
+											}
 
-			</div><!-- container --> 
+											?>
 
-		</div><!-- layoutSidenav --> 
+										</tbody>
+									</table> <!-- table table-bordered table-hover -->
 
-		<?php include "inc/admin_footer.php" ?>      
+								</div><!-- alignment -->
+							</div><!-- row -->
+						</div><!-- container fluid -->
+					</main><!-- main -->
+
+				</div> <!-- content -->
+
+			</div><!-- row -->
+
+		</div><!-- container -->
+
+	</div><!-- layoutSidenav -->
+
+	<?php include "inc/admin_footer.php" ?>
--- inc/login.php
+++ inc/login.php
@@ -1,41 +1,43 @@
-<?php 
+<?php
 
 include "db.php";
 
-session_start(); 
+session_start();
 
 ?>
 
-<?php 
+<?php
 
 // if login button is clicked
-if(isset($_POST['login'])){
-  
+if (isset($_POST['login'])) {
+
   //echo "found"; //TEST - if the below message is displaying
 
   //catch user login details and clean Escapes special characters in a string for use in an SQL query, taking into account the current character set of the connection.
 
-  $login_email				  = $connection -> real_escape_string($_POST['login_email']);
-  $login_password				= $connection -> real_escape_string($_POST['login_password']);
+  $login_email          = $connection->real_escape_string($_POST['login_email']);
+  $login_password       = $connection->real_escape_string($_POST['login_password']);
 
-  // SELECT all from TABLE WHERE COLUMN MATCHES USER INPUT FROM FORM. 
-  $login_user = $connection->query("SELECT * FROM users WHERE  user_email = '{$login_email}' "); 
+  $query = "SELECT * FROM users WHERE  user_email = ?";
+  $statement = mysqli_prepare($connection, $query);
+  mysqli_stmt_bind_param($statement, 's', $login_email);
+  mysqli_stmt_execute($statement);
+  $result = mysqli_stmt_get_result($statement);
 
-  if($login_user ->num_rows > 0) {
-    foreach($login_user as $row ){
+  if ($result->num_rows > 0) {
+    foreach ($result as $row) {
 
       $db_id = $row['userId'];  // test OUTPUT - id is there
-      $db_userId = $row['userId']; 
-      $db_user_firstname = $row['user_firstname']; 
+      $db_userId = $row['userId'];
+      $db_user_firstname = $row['user_firstname'];
       $db_username = $row['username'];  // OUTPUT
-      $db_user_password = $row['user_password'];   
-      $db_user_lastname = $row['user_lastname']; 
+      $db_user_password = $row['user_password'];
+      $db_user_lastname = $row['user_lastname'];
       $db_user_email = $row['user_email'];
-      $db_user_role = $row['user_role']; 
-
+      $db_user_role = $row['user_role'];
     }
 
-    if(password_verify($login_password,$row['user_password'])){
+    if (password_verify($login_password, $row['user_password'])) {
       //echo "You have been logged in";
       $_SESSION['user_firstname'] = $db_user_firstname;
       $_SESSION['username'] = $db_username;
@@ -43,21 +45,16 @@ if(isset($_POST['login'])){
       $_SESSION['user_lastname'] = $db_user_lastname;
       $_SESSION['user_email'] = $db_user_email;
       $_SESSION['user_role'] = $db_user_role;
-    
+
       header('Location: ../admin/posts.php');
-
     } else {
-    
-      header("Location: ../index.php");
-    
-    }
 
+      header("Location: ../index.php");
+    }
   } else {
 
     echo "please check your inputs";
-  
   }
-
 } // isset 
 
 ?>
