8e01daf8482fa57f7909f1ed130926ee549a320d
krystle Mensah <web-development@krystlemensah.co.uk>
2021-02-20 18:15:51 +0000

    select user password prepared statement

--- admin/inc/edit_user.php
+++ admin/inc/edit_user.php
@@ -1,16 +1,21 @@
-<?php 
+<?php
 // IF SET
-if(isset($_GET['edit_user'])){
+if (isset($_GET['edit_user'])) {
   // then save in this varible
   $the_user_id = $_GET['edit_user'];
 
-  // select all from table where the user id is equal to the url id
-  $query = "SELECT * FROM users WHERE userId = $the_user_id ";
+  //SELECT ALL BY
+  $query = "SELECT * FROM users WHERE userId = ?";
+
+  $statement = mysqli_prepare($connection, $query);
+  mysqli_stmt_bind_param($statement, 'i', $the_user_id);
+  mysqli_stmt_execute($statement);
+  $select_users_query = mysqli_stmt_get_result($statement);
 
   // then Perform query against a database and send in the query and the connection
-  $select_users_query = mysqli_query($connection,$query);
+  //$select_users_query = mysqli_query($connection, $query);
 
-  while($row = mysqli_fetch_array($select_users_query)) {
+  while ($row = mysqli_fetch_array($select_users_query)) {
 
     // values we bring back and assign to variable
     $userId         = $row['userId'];
@@ -24,7 +29,7 @@ if(isset($_GET['edit_user'])){
 }
 
 // IF PRESSED
-if(isset($_POST['edit_user'])){
+if (isset($_POST['edit_user'])) {
   // PICK UP VAULES
   $user_firstname        = $_POST['user_firstname'];
   $user_lastname         = $_POST['user_lastname'];
@@ -32,50 +37,53 @@ if(isset($_POST['edit_user'])){
   $username              = $_POST['username'];
   $user_email            = $_POST['user_email'];
   $user_password         = $_POST['user_password'];
- 
-  
-  if(!empty($user_password)) { 
+
+
+  if (!empty($user_password)) {
+
 
     $query_password = "SELECT user_password FROM users WHERE userId =  $the_user_id";
-    
     $get_user_query = mysqli_query($connection, $query_password);
-    
     confirmQuery($get_user_query);
-
     $row = mysqli_fetch_array($get_user_query);
-
     $db_user_password = $row['user_password'];
 
+    //Fetch Single record without bind_result:
+    // $qry_password = "SELECT user_password FROM users WHERE userId =  $the_user_id";
+    // $statement = mysqli_prepare($connection, $qry_password);
+    // mysqli_stmt_bind_param($statement, 'i', $the_user_id);
+    // mysqli_stmt_execute($statement);
+    // $result = mysqli_stmt_get_result($statement);
+    // $getData = mysqli_fetch_assoc($result);
+    // $db_user_password = $row['user_password'];
 
-    if($db_user_password != $user_password) {
+
+    if ($db_user_password != $user_password) {
 
       $hashed_password = password_hash($user_password, PASSWORD_BCRYPT, array('cost' => 12));
-
     }
-  
-  
-  // UPDATE DATABASE
-  $query = "UPDATE users SET user_firstname = '{$user_firstname}', user_lastname = '{$user_lastname}', 
+
+
+    // UPDATE DATABASE
+    $query = "UPDATE users SET user_firstname = '{$user_firstname}', user_lastname = '{$user_lastname}', 
   user_role = '{$user_role}', username = '{$username}', user_email = '{$user_email}', 
   user_password = '{$hashed_password}' WHERE userId = {$the_user_id} ";
 
-  //SEND IT IN
-  $edit_user_query = mysqli_query($connection, $query);
-  
-  // CONFIRM QUERY
-  confirmQuery($edit_user_query);
-  
-  // display this
-  echo "<p class='success-button'>User Updated. <a href='users.php'>View Users</a></p>";
-  
-  }
+    //SEND IT IN
+    $edit_user_query = mysqli_query($connection, $query);
 
+    // CONFIRM QUERY
+    confirmQuery($edit_user_query);
+
+    // display this
+    echo "<p class='success-button'>User Updated. <a href='users.php'>View Users</a></p>";
+  }
 }
 
 ?>
 
 <!-- multipart/form-data lets you send encoded data  -->
-<form action="" method="post" enctype="multipart/form-data"> 
+<form action="" method="post" enctype="multipart/form-data">
 
   <div class="form-group">
     <label for="title">First Name</label>
@@ -84,27 +92,25 @@ if(isset($_POST['edit_user'])){
 
   <div class="form-group">
     <label for="title">Last Name</label>
-    <input type="text" value="<?= $user_lastname; ?>"  class="form-control" name="user_lastname">
+    <input type="text" value="<?= $user_lastname; ?>" class="form-control" name="user_lastname">
   </div>
 
   <div class="form-group">
-   
+
     <select name="user_role" id="">
       <!-- static data added -->
       <option value="subscriber"><?= $user_role; ?></option>
 
-      <?php if($user_role == 'admin'){
+      <?php if ($user_role == 'admin') {
 
-      echo  "<option value='subscriber'>subscriber</option>";
-
+        echo  "<option value='subscriber'>subscriber</option>";
       } else {
 
         echo "<option value='admin'>admin</option>";
-
-      }?>
+      } ?>
 
     </select>
-    
+
   </div> <!-- form-group -->
 
   <!-- <div class="form-group">
