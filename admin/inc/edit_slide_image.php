ee9360533b4a0ac1414f9b05160b6115323bafdf
krystle Mensah <web-development@krystlemensah.co.uk>
2021-02-20 15:14:11 +0000

    edit page select statments changes

--- admin/inc/edit_slide_image.php
+++ admin/inc/edit_slide_image.php
@@ -1,19 +1,20 @@
 <?php
 
 //if the post id is set in the url
-if(isset($_GET['GET_carousel_id'])){
-  
+if (isset($_GET['GET_carousel_id'])) {
+
   //GET it and save it in this variable
   $GET_carousel_id = $_GET['GET_carousel_id'];
 }
 
-$query = "SELECT * FROM carousel WHERE carousel_id = $GET_carousel_id  ";
+$query = "SELECT * FROM carousel WHERE carousel_id = ? ";
 
-// mysqli_query function sends in the above query and connection. 
-$select_carousel_by_id = mysqli_query($connection,$query);
+$statement = mysqli_prepare($connection, $query);
+mysqli_stmt_bind_param($statement, 'i', $GET_carousel_id);
+mysqli_stmt_execute($statement);
+$select_carousel_by_id = mysqli_stmt_get_result($statement);
 
-//condition is true fetch the row representing the array from ($variable)
-while($row = mysqli_fetch_array($select_carousel_by_id)) {
+while ($row = mysqli_fetch_array($select_carousel_by_id)) {
 
   // ARRAY values we bring back and assign to variable
   $slide_id          = $row['carousel_id'];
@@ -22,12 +23,11 @@ while($row = mysqli_fetch_array($select_carousel_by_id)) {
   $slide_author      = $row['carousel_author'];
   $slide_image       = $row['carousel_image'];
   $slide_tags        = $row['carousel_tags'];
-  $slide_content     = $row['carousel_content'];  
+  $slide_content     = $row['carousel_content'];
   $slide_date        = $row['carousel_date'];
-  
 }
 
-if(isset($_POST['update_slide'])){
+if (isset($_POST['update_slide'])) {
   //echo "hi";
 
   $slide_title          =  $_POST['carousel_title'];
@@ -37,35 +37,45 @@ if(isset($_POST['update_slide'])){
   $slide_image_temp     =  $_FILES['image']['tmp_name'];
   $slide_content        =  $_POST['carousel_content'];
   $slide_tags           =  $_POST['carousel_tags'];
-  
+
   move_uploaded_file($slide_image_temp, "../img/$slide_image");
 
   // if empty var
-  if(empty($slide_image)) {
-    
+  if (empty($slide_image)) {
+
     //select all where col id = var
-    $query = "SELECT * FROM carousel WHERE carousel_id = $GET_carousel_id ";
-    $select_image = mysqli_query($connection,$query);
+    $query = "SELECT * FROM carousel WHERE carousel_id = ?";
 
-    // then loop though result set
-    while($row = mysqli_fetch_array($select_image)) {
-            
+    $statement = mysqli_prepare($connection, $query);
+    mysqli_stmt_bind_param($statement, 'i', $GET_carousel_id);
+    mysqli_stmt_execute($statement);
+    $select_image = mysqli_stmt_get_result($statement);
+
+    while ($row = mysqli_fetch_array($select_image)) {
+
       $slide_image = $row['carousel_image'];
-  
     }
-
   }
 
-  // update post then set each column in the database table equal to variable the form.
-  $query = "UPDATE carousel SET carousel_title  = '{$slide_title}', carousel_author = '{$slide_author}', carousel_cat_id = '{$slide_category_id}', carousel_date   =  now(), 
-  carousel_tags = '{$slide_tags}', carousel_content = '{$slide_content}', carousel_image = '{$slide_image}' WHERE carousel_id  = {$GET_carousel_id} ";
+  $updateQry = "UPDATE carousel SET carousel_title  = ?, carousel_author  = ?, carousel_cat_id  = ?, carousel_date  = ?, post_status  = ?, carousel_tags  = ?, carousel_content  = ?, carousel_image  = ? WHERE carousel_id = ?";
 
-  // Then we send in the query
-  $update_slide = mysqli_query($connection,$query);
-  
-  // confirm update post
-  confirmQuery($update_slide);
-  
+  $updateStatement = mysqli_prepare($connection, $updateQry);
+
+  mysqli_stmt_bind_param($updateStatement, 'ssssssssi', $slide_title, $slide_author, $slide_category_id, $post_date, $slide_tags, $post_tags, $slide_content, $slide_image, $GET_carousel_id);
+  mysqli_stmt_execute($updateStatement);
+
+  mysqli_close($connection);
+
+  // update post then set each column in the database table equal to variable the form.
+  // $query = "UPDATE carousel SET carousel_title  = '{$slide_title}', carousel_author = '{$slide_author}', carousel_cat_id = '{$slide_category_id}', carousel_date   =  now(), 
+  // carousel_tags = '{$slide_tags}', carousel_content = '{$slide_content}', carousel_image = '{$slide_image}' WHERE carousel_id  = {$GET_carousel_id} ";
+
+  // // Then we send in the query
+  // $update_slide = mysqli_query($connection, $query);
+
+  // // confirm update post
+  // confirmQuery($update_slide);
+
   // display this
   //echo "<p class='success-button'>Post Updated. <a href='../post.php?p_id={$the_post_id}'>View Post </a> or <a href='posts.php'>Edit More Posts</a></p>";
 
@@ -74,7 +84,7 @@ if(isset($_POST['update_slide'])){
 ?>
 
 <!-- EDIT SLIDE FORM  -->
-<form action="" method="post" enctype="multipart/form-data"> 
+<form action="" method="post" enctype="multipart/form-data">
 
   <div class="form-group">
     <label for="title">Title</label>
@@ -82,37 +92,34 @@ if(isset($_POST['update_slide'])){
   </div>
 
   <div class="form-group">
-    
+
     <select name="carousel_cat_id">
 
-      <!-- // SELECT ALL CATEGORIES QUERY -->
       <?php
-          
-          $query = "SELECT * FROM categories ";
-          $select_categories = mysqli_query($connection,$query);
-          confirmQuery($select_categories);
+      // SELECT ALL CATEGORIES QUERY
+      $query = "SELECT * FROM categories";
 
-          while($row = mysqli_fetch_assoc($select_categories )) {
-          $cat_id = $row['cat_id'];
-          $cat_title = $row['cat_title'];
+      $statement = mysqli_prepare($connection, $query);
+      mysqli_stmt_execute($statement);
+      $select_categories = mysqli_stmt_get_result($statement);
 
+      while ($row = mysqli_fetch_assoc($select_categories)) {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
 
-          if($cat_id == $slide_category_id) {
+        if ($cat_id == $slide_category_id) {
 
-            echo "<option selected value='{$cat_id}'>{$cat_title}</option>";
+          echo "<option selected value='{$cat_id}'>{$cat_title}</option>";
+        } else {
 
-          } else {
-
-            echo "<option value='{$cat_id}'>{$cat_title}</option>";
-
-          }
-              
-          }
+          echo "<option value='{$cat_id}'>{$cat_title}</option>";
+        }
+      }
 
       ?>
-    
+
     </select>
-    
+
   </div> <!-- form-group -->
 
   <div class="form-group">
@@ -122,8 +129,8 @@ if(isset($_POST['update_slide'])){
 
   <div class="form-group">
     <label for="image">Post Image</label>
-    <img width="100" src="../img/<?php echo $slide_image; ?>" alt="">
-    <input type="file"  name="image">
+    <img width="100" src="../img/<?= $slide_image; ?>" alt="">
+    <input type="file" name="image">
   </div>
 
   <div class="form-group">
@@ -133,7 +140,7 @@ if(isset($_POST['update_slide'])){
 
   <div class="form-group">
     <label for="slide_content">Post Content</label>
-    <textarea class="form-control "name="carousel_content" id="body" cols="30" rows="10"><?= $slide_content;?></textarea>
+    <textarea class="form-control " name="carousel_content" id="body" cols="30" rows="10"><?= $slide_content; ?></textarea>
   </div>
   <script>
     CKEDITOR.replace('carousel_content');
