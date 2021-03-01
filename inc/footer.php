59648738e247916229a670704376583087320c07
krystle Mensah <web-development@krystlemensah.co.uk>
2021-03-01 15:25:42 +0000

    footer tidy

--- inc/footer.php
+++ inc/footer.php
@@ -3,16 +3,16 @@
     <a href="https://open.spotify.com/user/3a91knedfbhntn7akthnp5l84?si=mDkzbZv_T6WLzbwC9FkXmQ" target="_blank" class="footerSocial_link"><i class="fab fa-spotify"></i></a>
     <a href="https://soundcloud.com/inspired-sound-167086100" target="_blank" class="footerSocial_link"><i class="fab fa-soundcloud"></i></a>
 
-      <!-- <p>Kofi Yeboah Mensah <a href="#">Inspired Sound</a></p> -->
-      <p>
-        <a class="back-to-the-top" href="#">Back to top</a>
-      </p>
-  </footer>
-  <!-- jquery this needs to go in the footer as well-->
+    <!-- BACK TO THE TOP -->
+    <p>
+      <a class="back-to-the-top" href="#">Back to top</a>
+    </p>
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <!-- jquery this needs to go in the footer as well-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
-<script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
-</body>
-</html>
+    </body>
+
+    </html>
