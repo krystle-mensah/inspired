dee2848027b25620d81a028d8453c1da5d4d2039
krystle Mensah <web-development@krystlemensah.co.uk>
2021-03-21 11:43:48 +0000

    mc signup form

--- inc/footer.php
+++ inc/footer.php
@@ -3,6 +3,25 @@
     <a href="https://open.spotify.com/user/3a91knedfbhntn7akthnp5l84?si=mDkzbZv_T6WLzbwC9FkXmQ" target="_blank" class="footerSocial_link"><i class="fab fa-spotify"></i></a>
     <a href="https://soundcloud.com/inspired-sound-167086100" target="_blank" class="footerSocial_link"><i class="fab fa-soundcloud"></i></a>
 
+    <!-- Begin Mailchimp Signup Form -->
+    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+    <div class="container">
+      <div id="mc_embed_signup">
+        <form action="https://inspiredsound.us20.list-manage.com/subscribe/post?u=490aaa11d646338f7afcf7959&amp;id=e47d0241f4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div id="mc_embed_signup_scroll">
+            <label for="mce-EMAIL">SIGN UP TO OUR NEWSLETTERS</label>
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_490aaa11d646338f7afcf7959_e47d0241f4" tabindex="-1" value=""></div>
+            <div class="clear">
+              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <!--End mc_embed_signup-->
+
     <!-- BACK TO THE TOP -->
     <div>
       <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fas fa-arrow-up"></i></button>
