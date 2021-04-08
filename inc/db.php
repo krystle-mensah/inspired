a6289e85d1ea5945880bb4ea803a29098661bf71
krystle Mensah <web-development@krystlemensah.co.uk>
2021-04-08 12:00:30 +0100

    added production enviroment

--- inc/db.php
+++ inc/db.php
@@ -1,9 +1,15 @@
 <?php
-
+// DEVELOPEMENT CONNECTION
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', "inspired");
+
+//REMOTE DATABASE CONNECTION
+//define('DB_HOST', 'sql4.freemysqlhosting.net');
+//define('DB_USER', 'root');
+//define('DB_PASS', 'bj2QV51Ry3');
+//define('DB_NAME', "sql4404106");
 
 // Create connection
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
