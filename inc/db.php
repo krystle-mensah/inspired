dc3a2dd4925f8f8243f102c4b800c9ebc994d860
krystle Mensah <web-development@krystlemensah.co.uk>
2021-04-08 17:59:50 +0100

    database update

--- inc/db.php
+++ inc/db.php
@@ -1,15 +1,15 @@
 <?php
 // DEVELOPEMENT CONNECTION
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', "inspired");
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', "inspired");
 
 //REMOTE DATABASE CONNECTION
-//define('DB_HOST', 'sql4.freemysqlhosting.net');
-//define('DB_USER', 'root');
-//define('DB_PASS', 'bj2QV51Ry3');
-//define('DB_NAME', "sql4404106");
+define('DB_HOST', 'sql4.freesqldatabase.com');
+define('DB_USER', 'sql4404292');
+define('DB_PASS', 'lRB4cfmAVD');
+define('DB_NAME', "sql4404292");
 
 // Create connection
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
