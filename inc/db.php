743d352da7df5f4b6049a5f930a86945caf7fbac
krystle Mensah <web-development@krystlemensah.co.uk>
2021-04-09 10:13:06 +0100

    checking connection

--- inc/db.php
+++ inc/db.php
@@ -14,9 +14,9 @@ define('DB_NAME', "sql4404292");
 // Create connection
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-// if (mysqli_connect_errno()) {
-//   printf("Connect failed: %s\n", mysqli_connect_error());
-//   exit();
-// } else {
-//   //echo "Connected successfully";
-// }
+if (mysqli_connect_errno()) {
+  printf("Connect failed: %s\n", mysqli_connect_error());
+  exit();
+} else {
+  echo "Connected successfully";
+}
