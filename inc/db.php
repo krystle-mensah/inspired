2648b2d54168d96a928667f647e0872218d8b501
krystle Mensah <web-development@krystlemensah.co.uk>
2021-04-09 11:24:43 +0100

    db update

--- inc/db.php
+++ inc/db.php
@@ -6,13 +6,29 @@
 // define('DB_NAME', "inspired");
 
 //REMOTE DATABASE CONNECTION
-define('DB_HOST', 'sql4.freesqldatabase.com');
-define('DB_USER', 'sql4404292');
-define('DB_PASS', 'lRB4cfmAVD');
-define('DB_NAME', "sql4404292");
+// define('DB_HOST', 'sql4.freesqldatabase.com');
+// define('DB_USER', 'sql4404292');
+// define('DB_PASS', 'lRB4cfmAVD');
+// define('DB_NAME', "sql4404292");
 
 // Create connection
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+//$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// if (mysqli_connect_errno()) {
+//   printf("Connect failed: %s\n", mysqli_connect_error());
+//   exit();
+// } else {
+//   echo "Connected successfully";
+// }
+
+$url = parse_url(getenv("mysql://ba381f6aafda8b:f9d0a3ec@us-cdbr-east-03.cleardb.com/heroku_95220712da5e428?reconnect=true"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$connection = mysqli_connect($server, $username, $password, $db);
 
 if (mysqli_connect_errno()) {
   printf("Connect failed: %s\n", mysqli_connect_error());
