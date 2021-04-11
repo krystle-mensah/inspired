277a74f00c2f624de61f32f7c7a50f67ceb4c866
krystle Mensah <web-development@krystlemensah.co.uk>
2021-04-11 17:58:54 +0100

    production server test

--- inc/db.php
+++ inc/db.php
@@ -1,22 +1,28 @@
 <?php
 // DEVELOPEMENT CONNECTION
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', "inspired");
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', "inspired");
 
 //REMOTE DATABASE CONNECTION
-// define('DB_HOST', 'sql4.freesqldatabase.com');
-// define('DB_USER', 'sql4404292');
-// define('DB_PASS', 'lRB4cfmAVD');
-// define('DB_NAME', "sql4404292");
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 // Create connection
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+//$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-// if (mysqli_connect_errno()) {
-//   printf("Connect failed: %s\n", mysqli_connect_error());
-//   exit();
-// } else {
-//   echo "Connected successfully";
-// }
+if (mysqli_connect_errno()) {
+  printf("Connect failed: %s\n", mysqli_connect_error());
+  exit();
+} else {
+  echo "Connected successfully";
+}
