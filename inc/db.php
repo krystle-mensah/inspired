24abebf763479f474a8aa04fb1a21e59667a5c84
krystle Mensah <web-development@krystlemensah.co.uk>
2021-04-12 19:14:07 +0100

    db update

--- inc/db.php
+++ inc/db.php
@@ -1,28 +1,40 @@
 <?php
-// DEVELOPEMENT CONNECTION
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'root');
-// define('DB_PASS', '');
-// define('DB_NAME', "inspired");
 
-//REMOTE DATABASE CONNECTION
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//echo gethostname();
+// Krystles-MBP.cust.communityfibre.co.uk
+if (gethostname() == "Krystles-MBP.cust.communityfibre.co.uk") {
+  // DEVELOPEMENT CONNECTION
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', "inspired");
 
-// Create connection
-//$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+  // Create connection
+  $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-if (mysqli_connect_errno()) {
-  printf("Connect failed: %s\n", mysqli_connect_error());
-  exit();
+  // if (mysqli_connect_errno()) {
+  //   printf("Connect failed: %s\n", mysqli_connect_error());
+  //   exit();
+  // } else {
+  //   //echo "Connected successfully";
+  // }
 } else {
-  echo "Connected successfully";
+  //REMOTE DATABASE CONNECTION
+  //Get Heroku ClearDB connection information
+  $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $cleardb_server = $cleardb_url["host"];
+  $cleardb_username = $cleardb_url["user"];
+  $cleardb_password = $cleardb_url["pass"];
+  $cleardb_db = substr($cleardb_url["path"], 1);
+  $active_group = 'default';
+  $query_builder = TRUE;
+  // Connect to REMOTE DB
+  $connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+  if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+  } else {
+    //echo "Connected successfully";
+  }
 }
